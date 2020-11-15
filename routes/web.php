91cebe0f7183b062cf8127ccb10ae1e498e0d8ae
tekihei2317 <tekihei2317@gmail.com>
2020-11-15 17:12:38 +0900

    feat: 記事一覧の表示画面を作成

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 // return view('welcome');
 // });
 
-Route::get('/', 'ArticleController@index');
+Route::get('/', 'App\Http\Controllers\ArticleController@index');
