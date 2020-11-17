4d669a5de34d69c1e87f38b53e4ba2d952111f39
tekihei2317 <tekihei2317@gmail.com>
2020-11-17 15:00:33 +0900

    fix: 記事編集時に元の内容が入るように修正

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -32,6 +32,6 @@ class ArticleController extends Controller
 
     public function edit(Article $article)
     {
-        return view('article.edit', ['article' => $article]);
+        return view('articles.edit', ['article' => $article]);
     }
 }
